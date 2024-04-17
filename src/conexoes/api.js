6a7aeaad4ea266e752aaf62c51ac7da26e7541ff
EmersonSantana1994 <emerson1994vp@gmail.com>
2024-04-17 14:23:55 -0300

    r

--- src/conexoes/api.js
+++ src/conexoes/api.js
@@ -4,12 +4,12 @@ const config = require('../configuracoes.json')
 // AQUI É REALIZADO A COMUNICAÇÃO COM O BACK
 const ipC = config.m2.ipC;
 let token
-// const apiC = axios.create({
-//   baseURL: 'https://' + ipC,
-// });
 const apiC = axios.create({
-  baseURL: 'http://' + ipC,
+  baseURL: 'https://' + ipC,
 });
+// const apiC = axios.create({
+//   baseURL: 'http://' + ipC,
+// });
 
 function pegarToken() {
   token = JSON.parse(localStorage.getItem("keyToken"))
